f56d04e02ba18c7e55f8ce796535e1748615dfce
halimashemanto <suprovashemanto@gmail.com>
2025-04-06 19:02:47 +0600

    add positive &  grade number

--- promptAlertRadious/src/promptalertradious/PromptAlertRadious.java
+++ promptAlertRadious/src/promptalertradious/PromptAlertRadious.java
@@ -7,40 +7,36 @@ public class PromptAlertRadious {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-        // System.out.println("Enter Radious number.");
-
-        // float zeroNeg = s.nextFloat();
-        //System.out.println("Sorry, Zero & any Negative Number are not Valid Radious Number!! ");
-        //System.out.println("Please Enter Positive Radious Number.");
-        // int radius = s.nextInt();
-        // double pi = 3.142;
-        // double result = pi * (radius * radius);
-        //System.out.println("Area of circle is : " + result);
-  //}
-//}  
-
-
-// Another method IF ELSE 
-
-System.out.println("Enter Radious number.");
- float rad = s.nextFloat();
-    if (rad <=0) {
-       System.out.println("Sorry, Zero & any Negative Number are not Valid Radious Number!!");
-        System.out.println("please enter positive radious number");
-       float radius = s.nextFloat();
-       
+        System.out.println("Enter Radious number.");
+        float zeroNeg = s.nextFloat();
+  
+        System.out.println("Sorry, Zero & any Negative Number are not Valid Radious Number!! ");
+        System.out.println("Please Enter Positive Radious Number.");
+        int radius = s.nextInt();
         double pi = 3.142;
         double result = pi * (radius * radius);
-        
-       System.out.println(" Total Value is " + result);
-            
+        System.out.println("Area of circle is : " + result);
     }
-           
-    else {
-     System.out.println("It's not a radious number");
+}
+// Another method IF ELSE 
 
-    }
-    
-    
+//        System.out.println("Enter Radious number.");
+//
+//        if (rad <= 0) {
+//            System.out.println("Sorry, Zero & any Negative Number are not Valid Radious Number!!");
+//            System.out.println("please enter positive radious number");
+//            float rad = s.nextFloat();
+//
+//            float radius = s.nextFloat();
+//
+//            double pi = 3.142;
+//            double result = pi * (radius * radius);
+//
+//            System.out.println(" Total Value is " + result);
+//
+//        } else {
+//            System.out.println("It's not a radious number");
+//
+//        }
 }
 }
