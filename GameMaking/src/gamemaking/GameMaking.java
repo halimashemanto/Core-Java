aa039329928da8b2be59a7b62d1d43dd2b12e551
halimashemanto <suprovashemanto@gmail.com>
2025-04-07 18:39:11 +0600

    add

---
+++ GameMaking/src/gamemaking/GameMaking.java
@@ -0,0 +1,31 @@
+package gamemaking;
+
+import java.util.Scanner;
+
+public class GameMaking {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int lottery = (int) (Math.random() * 10);
+      
+        
+        
+        System.out.print("Enter your picking number ");
+        int guess = s.nextInt();
+
+          System.out.println(  "Random Number is " +lottery);
+        
+        if (guess == lottery) {
+            System.out.println("Win");
+        } 
+        
+        else {
+            System.out.println("Sorry");
+        }
+    }
+}
+
+
+
