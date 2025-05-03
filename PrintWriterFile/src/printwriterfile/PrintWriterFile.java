09748fea95ca65b554748054d7d066de68bae828
halimashemanto <suprovashemanto@gmail.com>
2025-05-03 18:58:08 +0600

    add

--- PrintWriterFile/src/printwriterfile/PrintWriterFile.java
+++ PrintWriterFile/src/printwriterfile/PrintWriterFile.java
@@ -10,27 +10,33 @@ public class PrintWriterFile {
 
     public static void main(String[] args) throws FileNotFoundException {
 
+
+        //eta best practice
+        File f = new File("C:\\Users\\Admin\\Desktop\\input.txt");
+
+        PrintWriter pw = new PrintWriter(f);
+        pw.println("Most popular language in the world.");
+
+        pw.close();
+
+        Scanner s = new Scanner(f);
+        System.out.println(s.nextLine());
+
+        while (s.hasNextLine()) {
+            System.out.println(s.nextLine());
+        }
+    }
+}
+
+// eta easy way
 //        try (PrintWriter pw = new PrintWriter("C:\\Users\\Admin\\Desktop\\input.txt")) {
 //            pw.println("""
 //                                   Java is high level programing language.
 //                                   """);
 //            pw.println("Most popular language in the world.");
 //        }
-//    }
 
 
-        File f = new File("C:\\Users\\Admin\\Desktop\\input.txt");
-        
-        PrintWriter pw = new PrintWriter(f);
-         pw.println("Most popular language in the world.");
-         
-         pw.close();
-         
-         Scanner s = new Scanner(f);
-         System.out.println(s.nextLine());
-         
-         while(s.hasNextLine()){
-           System.out.println(s.nextLine());
-         }
-}
-}
+
+
+
