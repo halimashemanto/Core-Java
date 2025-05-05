3ea510f98b6610346eb4fe99612d79624d3d3da7
halimashemanto <suprovashemanto@gmail.com>
2025-05-05 19:06:59 +0600

    hoyni

--- MultiDiamantionalArraysWithScnnar/src/multidiamantionalarrayswithscnnar/MultiDiamantionalArraysWithScnnar.java
+++ MultiDiamantionalArraysWithScnnar/src/multidiamantionalarrayswithscnnar/MultiDiamantionalArraysWithScnnar.java
@@ -1,46 +1,36 @@
-
 package multidiamantionalarrayswithscnnar;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Scanner;
 
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class MultiDiamantionalArraysWithScnnar {
 
     public static void main(String[] args) {
-        
+
         Scanner s = new Scanner(System.in);
 
-        
         System.out.print("Enter number of rows.");
         int rows = s.nextInt();
         System.out.print("Enter number of columns.");
         int cols = s.nextInt();
 
-        
         int[][] arr = new int[rows][cols];
         System.out.println("Enter elements row by rows.");
+        int temp;
+          int[] array =null;
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                arr[i][j] = s.nextInt();
+//                arr[i][j] = s.nextInt();
+                 
+               
+                temp = array[j + 1];
+                
+                    array[j + 1] = array[j];
+                    array[j] = temp;
             }
         }
-        
-        
-            Integer[][] boxedArr = Arrays.stream(arr)
-                .map(row -> Arrays.stream(row).boxed().toArray(Integer[]::new))
-                .toArray(Integer[][]::new);
-            
-              System.out.print("Enter column index to sort by (0-based): ");
-        int colIndex = s.nextInt();
-         Arrays.sort(boxedArr, Comparator.comparingInt(row -> row[colIndex]));
-         
-            System.out.println("Sorted array:");
-        for (Integer[] row : boxedArr) {
-            System.out.println(Arrays.toString(row));
-        }
-
+          System.out.println("Array After Sorting: " + Arrays.toString(array));
     }
-    
+
 }
